981f1c0461d798eb9deffa9ab472e08233b4a2ec
dchretien <dchretien@theicongroup.com>
2019-12-20 16:51:52 -0500

    Added basic autonomous code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1920.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Auto1920 extends superAutoNew {
+
+        public void runOpMode() {
+            setUp();
+            configVuforiaSkystone();
+
+
+            //Translate
+            translate(-1,0,.5, 0.5);
+            sR();
+
+
+
+        }
+
+}
