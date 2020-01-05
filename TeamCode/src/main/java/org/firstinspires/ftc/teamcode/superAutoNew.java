52d9e3b128b04726329b5b1a8ebde34dc6a39643
elotter13 <elotter13@gmail.com>
2020-01-05 17:16:32 -0500

    Added function to Super Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superAutoNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superAutoNew.java
@@ -1274,6 +1274,33 @@ abstract public class superAutoNew extends LinearOpMode {
         }
     }
 
+    void moveFoundation(double direction) {
+        //Raise arm a little
+        slideLifter.setPower(1);
+        Wait(0.5d);
+        slideLifter.setPower(0);
+
+        //Translate
+        translate(-1,0,3, 0.75);
+
+        //Reach out
+        reacher.setPower(1);
+        Wait(2.75d);
+        reacher.setPower(0);
+
+        //Set claw down
+        slideLifter.setPower(1);
+        Wait(0.5);
+        slideLifter.setPower(0);
+
+        //Translate
+        translate(1, 0, 3, 0.75);
+
+        //Back up to park under bridge
+        translate(0,1,2,0.75);
+
+    }
+
     enum pivotStates {None, Left, Right, Init}
 
     enum states {Space, Gold, Gold_Again, Silver, Silver_Again}
