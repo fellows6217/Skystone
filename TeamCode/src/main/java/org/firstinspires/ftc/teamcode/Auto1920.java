f66abf364b5d2f2ac9116e96100755c78cc09988
elotter13 <elotter13@gmail.com>
2020-01-05 16:53:52 -0500

    Added steps to Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1920.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1920.java
@@ -26,6 +26,17 @@ public class Auto1920 extends superAutoNew {
             Wait(2.75d);
             reacher.setPower(0);
 
+            //Set claw down
+            slideLifter.setPower(1);
+            Wait(0.5);
+            slideLifter.setPower(0);
+
+            //Translate
+            translate(1, 0, 3, 0.75);
+
+            //Back up to park under bridge
+            translate(0,1,2,0.75);
+
 
                sR();
 
