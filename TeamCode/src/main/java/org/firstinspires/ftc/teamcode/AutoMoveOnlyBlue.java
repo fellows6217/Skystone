4f08694339a317e3d47d9934b39f49b83be82d24
elotter13 <elotter13@gmail.com>
2020-01-05 17:27:45 -0500

    Added Blue Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMoveOnlyBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMoveOnlyBlue.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="Preciousss: Auto1920", group="Preciousss")
+@Autonomous(name="Preciousss: AutoMoveOnlyRed", group="Preciousss")
 
-public class Auto1920 extends superAutoNew {
+public class AutoMoveOnlyBlue extends superAutoNew {
 
         public void runOpMode() {
             setUp();
@@ -12,7 +12,7 @@ public class Auto1920 extends superAutoNew {
 
             playSound(14);
 
-            moveFoundation(1d);
+            moveFoundation(-1d);
 
             sR();
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMoveOnlyRed.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Preciousss: AutoMoveOnlyRed", group="Preciousss")
+
+public class AutoMoveOnlyRed extends superAutoNew {
+
+        public void runOpMode() {
+            setUp();
+            //configVuforiaSkystone();
+
+            playSound(14);
+
+            moveFoundation(1d);
+
+            sR();
+
+
+
+        }
+
+}
