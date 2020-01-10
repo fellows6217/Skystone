3047bbe157f406f135f04bbfeed57c7f4438b884
elotter13 <elotter13@gmail.com>
2020-01-09 20:55:49 -0500

    Added claw rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superAutoNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superAutoNew.java
@@ -1278,9 +1278,6 @@ abstract public class superAutoNew extends LinearOpMode {
 
         double sideSpeed=14;
 
-        //Rotate claw
-        claw.setPosition(0.9);
-
         //Raise arm a little
         slideLifter.setPower(1);
         Wait(0.5d);
@@ -1290,12 +1287,15 @@ abstract public class superAutoNew extends LinearOpMode {
         translate(direction,0,15/sideSpeed, 0.75);
 
         //Forward
-        translate(0, -direction,1.05,0.75);
+        translate(0, -direction,1.1,0.75);
 
         //Reach out
         reacher.setPower(1);
         Wait(1.375d);
         reacher.setPower(0);
+
+        //Rotate wrist
+        wrist.setPosition(0.9);
 
         //Set claw down
         slideLifter.setPower(-1);
@@ -1303,7 +1303,7 @@ abstract public class superAutoNew extends LinearOpMode {
         slideLifter.setPower(0);
 
         //Back up to drag foundation
-        translate(0,direction,1.05,0.75);
+        translate(0,direction,1.1,0.75);
 
         //Lift claw
         slideLifter.setPower(1);
